c3c7b7f292651117c7dc47d49841cf20890af0e7
Ashok Kumar <ashokkumar56624@gmail.com>
2023-05-04 17:56:32 +0530

    first commit

---
+++ frontend/src/components/NotFound.js
@@ -0,0 +1,18 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div className="center">
+            <h2>404 Not Found</h2>
+            <iframe src="https://giphy.com/embed/3h2AeAOj83j7slRkyW" 
+              width="374" 
+              height="480"
+              frameBorder="0"
+              class="giphy-embed" 
+              allowFullScreen></iframe>
+           
+        </div>
+    )
+}
+export default NotFound;
